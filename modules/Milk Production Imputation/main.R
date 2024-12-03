07e81d8c35ede7fd13d1d9fa6e1335e602208ada
Sebastian Campbell <sebastian-c@users.noreply.github.com>
2024-12-03 16:30:38 +0100

    Removed email from milk

--- modules/Milk Production Imputation/main.R
+++ modules/Milk Production Imputation/main.R
@@ -456,13 +456,13 @@ SaveData(domain = sessionKey@domain,
 #              data = completeTriplet)
 # }
 
-if(!CheckDebug()){
-    ## Initiate email
-    from = "sws@fao.org"
-    to = swsContext.userEmail
-    subject = "Milk production module"
-    body = paste0("Milk production module successfully ran. You can browse results in the session: ", sessionKey@sessionId )
-    sendmail(from = from, to = to, subject = subject, msg = body)
-}
+# if(!CheckDebug()){
+#     ## Initiate email
+#     from = "sws@fao.org"
+#     to = swsContext.userEmail
+#     subject = "Milk production module"
+#     body = paste0("Milk production module successfully ran. You can browse results in the session: ", sessionKey@sessionId )
+#     sendmail(from = from, to = to, subject = subject, msg = body)
+# }
 
 message("Module finished successfully")
